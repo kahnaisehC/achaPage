88bee91e54cced3b99a6520e570d11cab4c8a88b
Ian Hakanson <gabianhak@gmail.com>
2025-01-18 21:19:48 -0300

    docs: added documentation to chessboard

--- public/js/game.js
+++ public/js/game.js
@@ -1,28 +1,20 @@
-let p1_input = document.getElementById("playeroneinput");
-let p1_div = document.getElementById("playeronediv");
-let p1_button = document.getElementById("playeronebutton");
-let p2_input = document.getElementById("playertwoinput");
-let p2_div = document.getElementById("playertwodiv");
-let p2_button = document.getElementById("playertwobutton");
+const p1_input = document.getElementById("playeroneinput");
+const p1_div = document.getElementById("playeronediv");
+const p1_button = document.getElementById("playeronebutton");
+const p2_input = document.getElementById("playertwoinput");
+const p2_div = document.getElementById("playertwodiv");
+const p2_button = document.getElementById("playertwobutton");
 
-let squareCoordinateDisplay = document.getElementById(
+const squareCoordinateDisplay = document.getElementById(
   "squareCoordinateDisplay",
 );
 
 let ws;
 
-let gameCanvas = document.getElementById("canvas");
-let gameBackground = new Image();
+const chessboardDisplay = document.getElementById("chessboard");
+const gameBackground = new Image();
 gameBackground.src = "assets/horsey.jpg";
-let canvasWidth = gameCanvas.style.width;
-let canvasHeight = gameCanvas.style.height;
-let imageWidth = gameBackground.width;
-let imageHeight = gameBackground.height;
 
-class ChessGame {
-  constructor() {
-  }
-}
 class Square {
   x;
   y;
@@ -40,24 +32,32 @@ class Square {
     };
   }
 }
-let boardSquares = new Map();
 
-for (let colCoorASCII = "a".charCodeAt(0), i = 0; i < 8; colCoorASCII++, i++) {
-  for (let rowCoor = "1", j = 0; j < 8; rowCoor++, j++) {
-    let colCoor = String.fromCharCode(colCoorASCII);
-    let width = canvasWidth;
-    boardSquares.set(
-      colCoor.concat("", rowCoor),
-      new Square(
-        {
-          x: (width * j) / 8,
-          y: (width * i) / 8,
-          width: width / 8,
-        },
-      ),
-    );
+class ChessGame {
+  topLeftCoordinate = {
+    x: 0,
+    y: 0,
+  };
+  whiteToMove = true;
+  boardState = new Map();
+  // TODO: make manual hashmap
+  // NOTE: boardState represents state of the board as key value pairs:
+  // "a1": "bK" => means there is a black King on a1
+  //
+  previousMoves = [];
+  castling = (1 << 5) - 1;
+  // NOTE: bitwise representation of either players right to castle
+  // 1000 => white can castle queenside
+  // 0100 => white can castle kingside
+  // 0010 => black can castle queenside
+  // 0001 => black can castle kingside
+  // 1111 => everybody can castle anywhere
+
+  constructor() {
+    console.log(this.castling);
   }
 }
+chessBoard = new ChessGame();
 
 function getSquareCoordinates({ x, y, width }) {
   if ((typeof width) === "string" || width instanceof String) {
@@ -89,7 +89,7 @@ function getSquareCoordinates({ x, y, width }) {
     }
   }
 
-  let squareCoordinate = String.fromCharCode("h".charCodeAt() - retX + 1)
+  const squareCoordinate = String.fromCharCode("h".charCodeAt() - retX + 1)
     .concat(
       retY,
     );
@@ -97,29 +97,27 @@ function getSquareCoordinates({ x, y, width }) {
 }
 
 function createImage(pieceInitials) {
-  let exampleImage = document.createElement("img");
+  const exampleImage = document.createElement("img");
   exampleImage.src = `assets/${pieceInitials}.svg`;
   exampleImage.style.zIndex = 10;
   exampleImage.style.position = "relative";
+  exampleImage.style.top = 0;
+  exampleImage.style.left = 0;
+
   exampleImage.style.height = "80px";
   exampleImage.style.width = "80px";
-
-  exampleImage.addEventListener("drag");
+  exampleImage.draggable = "true";
+  exampleImage.addEventListener("drag", (e) => {
+    exampleImage.style.transform = `translate(x, y)`;
+    console.log(e);
+  });
 
   return exampleImage;
 }
 
-function initializeGame(canvas) {
-  canvas.innerHTML = "";
-  let blackPieces = [];
-  blackPieces.push(createImage("bP"));
-  blackPieces.push(createImage("bP"));
-  blackPieces.push(createImage("bP"));
-  blackPieces.push(createImage("bP"));
-  blackPieces.push(createImage("bP"));
-  blackPieces.push(createImage("bP"));
-  blackPieces.push(createImage("bP"));
-  blackPieces.push(createImage("bP"));
+function initializeGame(chessBoardDisplay) {
+  chessBoardDisplay.innerHTML = "";
+  const blackPieces = [];
   blackPieces.push(createImage("bR"));
   blackPieces.push(createImage("bN"));
   blackPieces.push(createImage("bB"));
@@ -128,9 +126,25 @@ function initializeGame(canvas) {
   blackPieces.push(createImage("bB"));
   blackPieces.push(createImage("bN"));
   blackPieces.push(createImage("bR"));
+  blackPieces.push(createImage("bP"));
+  blackPieces.push(createImage("bP"));
+  blackPieces.push(createImage("bP"));
+  blackPieces.push(createImage("bP"));
+  blackPieces.push(createImage("bP"));
+  blackPieces.push(createImage("bP"));
+  blackPieces.push(createImage("bP"));
+  blackPieces.push(createImage("bP"));
 
-  let whitePieces = [];
+  const whitePieces = [];
 
+  whitePieces.push(createImage("wP"));
+  whitePieces.push(createImage("wP"));
+  whitePieces.push(createImage("wP"));
+  whitePieces.push(createImage("wP"));
+  whitePieces.push(createImage("wP"));
+  whitePieces.push(createImage("wP"));
+  whitePieces.push(createImage("wP"));
+  whitePieces.push(createImage("wP"));
   whitePieces.push(createImage("wR"));
   whitePieces.push(createImage("wN"));
   whitePieces.push(createImage("wB"));
@@ -139,40 +153,32 @@ function initializeGame(canvas) {
   whitePieces.push(createImage("wB"));
   whitePieces.push(createImage("wN"));
   whitePieces.push(createImage("wR"));
-  whitePieces.push(createImage("wP"));
-  whitePieces.push(createImage("wP"));
-  whitePieces.push(createImage("wP"));
-  whitePieces.push(createImage("wP"));
-  whitePieces.push(createImage("wP"));
-  whitePieces.push(createImage("wP"));
-  whitePieces.push(createImage("wP"));
-  whitePieces.push(createImage("wP"));
 
-  canvas.append(...whitePieces);
+  chessBoardDisplay.append(...blackPieces);
   for (let i = 0; i < 32; i++) {
-    canvas.appendChild(document.createElement("div"));
+    chessBoardDisplay.appendChild(document.createElement("div"));
   }
-  canvas.append(...blackPieces);
+  chessBoardDisplay.append(...whitePieces);
 }
 
-gameCanvas.addEventListener("mousemove", (e) => {
+gamechessBoardDisplay.addEventListener("mousemove", (e) => {
   squareCoordinateDisplay.innerHTML = getSquareCoordinates({
     x: e.offsetX,
     y: e.offsetY,
-    width: canvasWidth,
+    width: chessBoardDisplayWidth,
   });
 });
 
-initializeGame(gameCanvas);
+initializeGame(gamechessBoardDisplay);
 
 function connect() {
   ws = new WebSocket("/ws");
-  ws.onopen = function () {
+  ws.onopen = function() {
     console.log("Connected or something");
   };
 
-  ws.onmessage = function (event) {
-    let information = event.data;
+  ws.onmessage = function(event) {
+    const information = event.data;
     if (information[1] == "1") {
       p1_div.innerHTML += `<p>${event.data}\n</p>`;
     } else {
@@ -180,24 +186,24 @@ function connect() {
     }
   };
 
-  ws.onclose = function () {
+  ws.onclose = function() {
     console.log("Trying to reconnect...");
     setTimeout(connect, 1000);
   };
 
-  ws.onerror = function (error) {
+  ws.onerror = function(error) {
     console.error("WebSocket error:", error);
   };
 }
 
-p1_button.addEventListener("click", function () {
-  let input = p1_input.value;
+p1_button.addEventListener("click", function() {
+  const input = p1_input.value;
   ws.send("p1" + input);
   p1_input.value = "";
 });
 
-p2_button.addEventListener("click", function () {
-  let input = p2_input.value;
+p2_button.addEventListener("click", function() {
+  const input = p2_input.value;
   ws.send("p2" + input);
   p2_input.value = "";
 });
